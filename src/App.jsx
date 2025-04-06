1985098ecd97facb17867280b6e5584c8bbdcb76
vlaus9 <poroxnenko_official@bk.ru>
2025-04-06 23:04:28 +0300

    Final

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faXmark, faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import imageNice from './assets/znakomstvo.png'
+import imageFace from './assets/Face.png'
 
 
 export default function App() {
@@ -15,14 +16,14 @@ export default function App() {
   const [isLoadCircle, SetisLoadCircle] = useState(false);
   const [isTouchCards, SetisTouchCards] = useState(false);
   
-
+     
   useEffect(() => {
     Setisload(true)
   }, []);
 
   useEffect(() => {
    SetisActiveBlurCards(isHovered)
-  }, [isHovered]);
+  }, [isHovered]);//ПРИ НАВЕДЕНИИ НА КАРТОЧКИ ПОЯВЛЯЕТСЯ ЛЕГКИЙ БЛЮР
 
   const cardsClick = () => {
     SetisClickCards(true);
@@ -51,33 +52,47 @@ return () => clearTimeout(touchCards)
 
 
 
-
   return (
     <>
       <div className='body'>
 
 
-        <section className='header-left'>
+        <section className='header'>
 
-          <motion.p 
-          variants={variantName}
-          initial='initial' 
-          animate='animate'
-          transition={{duration:0.5, delay:0.4, ease: 'easeOut'}}
-          className='name'>
-          ПОРОХНЕНКО ВЛАДИСЛАВ ВАЛЕРЬЕВИЧ
-          </motion.p>
+          <div className='header-text'> 
+            <motion.p 
+            variants={variantName}
+            initial='initial' 
+            animate='animate'
+            transition={{duration:0.5, delay:0.4, ease: 'easeOut'}}
+            className='name'>
+            ПОРОХНЕНКО ВЛАДИСЛАВ ВАЛЕРЬЕВИЧ
+            </motion.p>
 
-          <motion.p 
-          variants={variantName}
-          initial='initial' 
-          animate='animate'
-          transition={{duration:0.5, delay:0.6, ease: 'easeOut'}}
-          className='profession-name'>
-          FRONTEND разработчик
-          </motion.p>
+            <motion.p 
+            variants={variantName}
+            initial='initial' 
+            animate='animate'
+            transition={{duration:0.5, delay:0.6, ease: 'easeOut'}}
+            className='profession-name'>
+            Frontend разработчик
+            </motion.p>
+          </div>
+
+          <div className='image-face-parent'>
+            <motion.img 
+            variants={variantName}
+            initial={{opacity:0, x:50}}
+            animate={{opacity:1, x:0}}
+            transition={{duration:1.3, delay:1.5, ease: 'easeOut'}}
+            src={imageFace} className='image-face'>
+
+            </motion.img>
+          </div>
 
         </section>
+
+        
         
 
 
@@ -98,38 +113,39 @@ return () => clearTimeout(touchCards)
 
         
           <motion.div
-          className={'absolute left-[50%] translate-x-[-50%] translate-y-[10%] w-[200px] h-[200px]'}>
+          className={'absolute left-[50%] translate-x-[-50%] w-[200px] max-w-[22vh] aspect-square'}>
 
-            <motion.p
+            <motion.div
             initial={{opacity: 0, y: 30}}
             animate={{
               x: isHovered ? 52 : 0,
-              y: 0,
+              y: isHovered ? -30 : 0,
               rotate: isHovered ? 360 : 0,
               opacity: 1,  
             }}
             transition={isload ? {} : {duration: 0.5, delay: 1.8, ease: 'easeOut'}}
-            className={isClickCards ? 'invisible' : 'absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] w-[150px] h-[50px] text-center content-center font-[Revard] text-[#3a414f] z-[3]'}
+            className={isClickCards ? 'invisible' : 'my-info'}
             >
-            Обо мне
-            </motion.p>
+            <p>Обо мне</p>
+            </motion.div>
 
             <motion.div
             //КРУГ
             initial={{opacity: 0}}
             animate={{
               x: isHovered ? 52 : 0,
+              y: isHovered ? -30 : 0,
               opacity: 1
             }}
             transition={isload ? {} : {duration: 0.5, delay: 1.7, ease: 'easeOut'}}
-            className={isClickCards ? 'invisible' : 'absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] w-[150px] h-[150px] rounded-[50%] border-[1.3px] border-[#3a414f] z-[1]'}>
+            className={isClickCards ? 'invisible' : 'absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] w-[83%] h-[83%] rounded-[50%] border-[1.3px] border-[#3a414f] z-[1]'}>
             </motion.div>
           
             <motion.div
-            initial={{height: '150px'}}
+            initial={{height: '84%'}}
             animate={{height: '0px'}}
             transition={{duration: 1.3, delay: 2.3, ease: 'easeInOut'}}
-            className={isLoadCircle ? 'absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] bg-[#489f9c] w-[160px] z-[2]' : 'absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] bg-[#489f9c] w-[160px] opacity-[0] z-[2]'}>
+            className={isLoadCircle ? 'absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] bg-[#18b09a] w-[85%] z-[2]' : 'absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] bg-[#18b09a] w-[85%] opacity-[0] z-[2]'}>
             </motion.div>
 
 
@@ -157,23 +173,27 @@ return () => clearTimeout(touchCards)
 }
 
 const stylesCards = {
-  sectionCardsStart: 'absolute left-[50%] translate-x-[-50%] w-[25vh] h-[30vh] flex items-center z-[1]',
+  sectionCardsStart: 'relative left-[50%] translate-x-[-50%] w-[25vh] h-[40vh] flex items-center justify-center z-[1]',
   sectionCardsClick:'absolute top-[0] left-[0] right-[0] bottom-[0] flex flex-wrap gap-[15px] justify-around items-center z-[1] overflow-scroll',
 
 
   cardsParentStart:
-    'w-[30%] h-[40vh] flex justify-center items-center',
-  cardsParentClick: 'w-[450px] h-[750px] shrink-0 flex flex-wrap justify-center items-center',
+    'w-[100%] h-[40vh] flex justify-center items-center',
+  cardsParentClick() {
+    if(window.innerWidth < 470 || window.innerHeight < 730) {
+      return 'w-[95vw] h-[90vh] flex flex-[1 0 33%] flex-wrap justify-center items-center'
+    } else {return 'min-w-[450px] h-[90vh]  flex flex-[1 0 33%] flex-wrap justify-center items-center'}
+  } ,
 
 
-  cardsStartOne: 'absolute left-[50%] w-[25vh] h-[30vh] rounded-[15px] translate-x-[-50%] translate-y-[10%] bg-[#439d99] opacity-[0.6]',
-  cardsStartTwo: 'absolute left-[50%] w-[25vh] h-[30vh] rounded-[15px] translate-x-[-50%] translate-y-[10%] bg-[#439d99] opacity-[0.6]',
-  cardsStartThree: 'absolute left-[50%] w-[25vh] h-[30vh] rounded-[15px] translate-x-[-50%] translate-y-[10%] bg-[#439d99] opacity-[0.6]',
+  cardsStartOne: 'absolute left-[50%] w-[25vh] h-[30vh] rounded-[15px] translate-x-[-50%] bg-[#439d99] opacity-[0.6]',
+  cardsStartTwo: 'absolute left-[50%] w-[25vh] h-[30vh] rounded-[15px] translate-x-[-50%] bg-[#439d99] opacity-[0.6]',
+  cardsStartThree: 'absolute left-[50%] w-[25vh] h-[30vh] rounded-[15px] translate-x-[-50%] bg-[#439d99] opacity-[0.6]',
 
 
-  cardsStartOneHover: 'absolute left-[50%] w-[25vh] h-[30vh] rounded-[15px] border-[0.3px] border-black translate-x-[-50%] translate-y-[10%] bg-[#439d99] opacity-[0.6]',
-  cardsStartTwoHover: 'absolute left-[50%] w-[25vh] h-[30vh] rounded-[15px] border-[0.3px] border-black translate-x-[-50%] translate-y-[10%] bg-[#439d99] opacity-[0.6]',
-  cardsStartThreeHover: 'absolute left-[50%] w-[25vh] h-[30vh] rounded-[15px] border-[0.3px] border-black translate-x-[-50%] translate-y-[10%] bg-[#439d99] opacity-[0.6]',
+  cardsStartOneHover: 'absolute left-[50%] w-[25vh] h-[30vh] rounded-[15px] border-[0.3px] border-black translate-x-[-50%] bg-[#439d99] opacity-[0.6]',
+  cardsStartTwoHover: 'absolute left-[50%] w-[25vh] h-[30vh] rounded-[15px] border-[0.3px] border-black translate-x-[-50%] bg-[#439d99] opacity-[0.6]',
+  cardsStartThreeHover: 'absolute left-[50%] w-[25vh] h-[30vh] rounded-[15px] border-[0.3px] border-black translate-x-[-50%] bg-[#439d99] opacity-[0.6]',
 
 
   cardsClickOne: 'relative w-[95%] h-[95%] rounded-[15px] border-[0.3px] bg-[#439d99] opacity-[0.9] rotate-15 border-[#1d2f35] hover:bg-sky-900',
@@ -181,7 +201,13 @@ const stylesCards = {
   cardsClickThree: 'relative w-[95%] h-[95%] rounded-[15px] border-[#1d2f35] border-[0.3px] bg-white hover:bg-sky-700 opacity-[0.9] rotate-345',
 
 
-  cardsCloseBut: 'fixed scale-[1.6] right-[35px] z-1 top-[25px] text-[#17252A] hover:scale-[2] hover:text-[#385059] transition-all'
+  cardsCloseBut: 'fixed scale-[1.6] right-[35px] z-1 top-[25px] text-[#17252A] hover:scale-[2] hover:text-[#385059] transition-all',
+
+  textInfo() {
+    if(window.innerHeight < 710 && window.innerWidth < 500) {
+      return 'absolute flex flex-col gap-[20px] w-[80%] h-[80%] left-[10%] top-[10%] font-[Revard] text-[15px] hyphens-auto'
+    } else {return 'absolute flex flex-col gap-[20px] w-[80%] h-[80%] left-[10%] top-[10%] font-[Revard] text-[20px] 2xl:text-[25px] hyphens-auto'}
+  }
 }
 
 const variantName = {
@@ -193,16 +219,16 @@ const variantName = {
 const CardsOne = ({isHovered, isload, isClickCards}) =>{ 
         return (  
           <div
-          className={isClickCards ? stylesCards.cardsParentClick : 
+          className={isClickCards ? stylesCards.cardsParentClick() : 
             stylesCards.cardsParentStart}>
             <motion.div 
             initial={{opacity: 0, y: 100}}
             animate={{x: isHovered ? -50 : 0,
+                      y: isHovered ? -30 : 0,
                       scale: isHovered ? 1.1 : 1,
                       rotate: isHovered ? -30 : -15,
-                      background: isHovered ? '#55a6a3' : '#439d99',
+                      background: isHovered ? '#0fbaa3' : '#16af99',
                       opacity: isHovered ? 1 : 0.6,
-                      y:0,
                     }}
             transition={isload ? {} : {duration: 0.5, delay: 0.7, ease:'easeOut'}}
                 className={isClickCards ? stylesCards.cardsClickOne : 
@@ -216,12 +242,12 @@ const CardsOne = ({isHovered, isload, isClickCards}) =>{
 
 const CardsTwo = ({isHovered, isload, isClickCards}) => {
         return (
-          <div className={isClickCards ? stylesCards.cardsParentClick : stylesCards.cardsParentStart}>
+          <div className={isClickCards ? stylesCards.cardsParentClick() : stylesCards.cardsParentStart}>
           <motion.div
           initial={{opacity: 0, y: 100}} 
-          animate={{y: isHovered ? -50 : 0,
+          animate={{y: isHovered ? -80 : 0,
                     scale: isHovered ? 1.1 : 1,
-                    background: isHovered ? '#55a6a3' : '#439d99',  
+                    background: isHovered ? '#0fbaa3' : '#16af99',  
                     opacity: isHovered ? 1 : 0.6,
                   }}
           transition={isload ? {} : {duration: 0.5, delay: 1, ease: 'easeOut'}}
@@ -236,15 +262,15 @@ const CardsTwo = ({isHovered, isload, isClickCards}) => {
 
 const CardsThree = ({isHovered, isload, isClickCards}) => {
         return (
-          <div className={isClickCards ? stylesCards.cardsParentClick : stylesCards.cardsParentStart}>
+          <div className={isClickCards ? stylesCards.cardsParentClick() : stylesCards.cardsParentStart}>
           <motion.div 
           initial={{y: 100, opacity: 0}}
           animate={{x: isHovered ? 50 : 0,
+                    y: isHovered ? -30 : 0,
                     scale: isHovered ? 1.1 : 1,
                     rotate: isHovered ? 30 : 15,
-                    background: isHovered ? '#55a6a3' : '#439d99',
+                    background: isHovered ? '#0fbaa3' : '#16af99',
                     opacity: isHovered ? 1 : 0.6, 
-                    y: 0
                     }}
           transition={isload ? {} : {duration: 0.5, delay: 1.3, ease: 'easeOut'}}
               className={isClickCards ? stylesCards.cardsClickThree : isHovered ? stylesCards.cardsStartThreeHover : stylesCards.cardsStartThree}
@@ -270,53 +296,54 @@ const CardsBlur = ({isActiveBlurCards, isClickCards}) => {
 
 const TextInfoOne = () => {
     return (
-      <article className='absolute flex flex-col gap-[20px] w-[80%] h-[80%] left-[10%] top-[10%] font-[Revard] text-[20px] hyphens-auto' lang='RU'>
+      <section className={stylesCards.textInfo()} lang='RU'>
         <p><b>Дата рождения: </b>{myInfo.age}</p>
         <p><b>Место проживания: </b>{myInfo.location}</p>
         <p><b>Образование: </b>{myInfo.education}</p>
         <p><b>Семейное положение: </b>{myInfo.family}</p>
         <p><b>Увлечения: </b>{myInfo.hobbies}</p>
         <p><b>Личные качества: </b>{myInfo.personalQualities}</p>
-      </article>
+      </section>
     )
   }
 
 const TextInfoTwo = () => {
   return (
-    <article className='absolute flex flex-col gap-[20px] w-[80%] h-[80%] left-[10%] top-[10%] font-[Revard] text-[20px]'>
+    <section className={stylesCards.textInfo()}>
       <p><b>Стек:</b></p>
         <ul className='absolute left-[5.4%] top-[10%] w-[90%] flex flex-col gap-[20px] list-disc'>
           <li>HTML</li>
           <li>CSS</li>
           <li>JavaScript</li>
           <li>React</li>
+          <li>Vite</li>
           <li>Git</li>
           <li>Tailwind CSS</li>
           <li>Framer Motion</li>
-          <li>Основы UI/UX дизайна, <br />адаптивная верстка</li>
+          <li>Основы UI/UX дизайна</li>
         </ul>
-    </article>
+    </section>
   )
 }
 
 const TextInfoThree = () => {
   return (
-    <arcticle className='absolute flex flex-col gap-[20px] w-[80%] h-[80%] left-[10%] top-[10%] font-[Revard] text-[20px] hyphens-auto' lang='RU'>
+    <section className={stylesCards.textInfo()} lang='RU'>
       <p>Геодезист по образованию, архитектор-градостроитель по крайнему месту работы, фронденд разработчик сейчас и в будущем.<br /> Креативный, жизнерадостный, позитивный, дружелюбный, усердный, иногда слишком дотошный, творческий.<br /> Полон желания учиться, перенимать опыт, познавать новое, создавать.</p>
       <ul className='flex flex-col gap-[10px]'>
         <li><FontAwesomeIcon icon={faPhone} />&nbsp;&nbsp;&nbsp;8 (916)-848-31-61</li>
         <a><FontAwesomeIcon icon={faEnvelope} />&nbsp;&nbsp;&nbsp;poroxnenko_official@bk.ru</a>
       </ul>
       <img src={imageNice} />
-    </arcticle>
+    </section>
   )
 }
 
 
 const stylesBlur = {
-  blurUnActive: 'absolute w-[100vw] h-[100vh] translate-y-[-60%] backdrop-blur-xs bg-black/30 z-[0] opacity-[0] transition-all duration-300',
-  blurActive: 'absolute w-[100vw] h-[100vh] translate-y-[-60%] backdrop-blur-xs bg-black/30 z-[0] transition-all duration-300',
-  blurClickActive: 'absolute w-[100%] h-[100%] translate-y-[-60%] backdrop-blur-xl bg-black/50 z-[0] transition-all duration-300',
+  blurUnActive: 'fixed top-[0] left-[0] h-[100vh] w-[100vw]  backdrop-blur-xs bg-black/30 z-[0] opacity-[0] transition-all duration-300',
+  blurActive: 'fixed top-[0] left-[0] h-[100vh] w-[100vw] backdrop-blur-xs bg-black/30 z-[0] transition-all duration-300',
+  blurClickActive: 'fixed top-[0] left-[0] h-[100vh] w-[100vw] backdrop-blur-xl bg-black/50 z-[0] transition-all duration-300',
 }
 
 
